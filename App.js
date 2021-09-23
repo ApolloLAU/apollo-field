ebf8fe79419deef0c1bab0f556e43bfdc7d22700
ea1ik <ea1ik@icloud.com>
2021-09-23 13:48:34 +0300

    ble

--- App.js
+++ App.js
@@ -5,18 +5,21 @@ import {
   View,
   PermissionsAndroid,
   Button,
+  ScrollView,
 } from "react-native";
+import { BleManager } from "react-native-ble-plx";
 import RNBluetoothClassic, {
   BluetoothDevice,
 } from "react-native-bluetooth-classic";
-import { ScrollView } from "react-native-gesture-handler";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.manager = new BleManager();
     this.state = {
-      discovering: false,
+      scanning: false,
       pairing: false,
+
       connecting: false,
       devices: [],
       message: "No Message",
@@ -24,104 +27,103 @@ class App extends Component {
     };
   }
 
-  async requestAccessFineLocationPermission() {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: "Access fine location required for discovery",
-        message:
-          "In order to perform discovery, you must enable/allow " +
-          "fine location access.",
-        buttonNeutral: 'Ask Me Later"',
-        buttonNegative: "Cancel",
-        buttonPositive: "OK",
-      }
-    );
-    return granted === PermissionsAndroid.RESULTS.GRANTED;
-  }
+  // async requestAccessFineLocationPermission() {
+  //   const granted = await PermissionsAndroid.request(
+  //     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  //     {
+  //       title: "Access fine location required for discovery",
+  //       message:
+  //         "In order to perform discovery, you must enable/allow " +
+  //         "fine location access.",
+  //       buttonNeutral: 'Ask Me Later"',
+  //       buttonNegative: "Cancel",
+  //       buttonPositive: "OK",
+  //     }
+  //   );
+  //   return granted === PermissionsAndroid.RESULTS.GRANTED;
+  // }
 
-  async componentDidUpdate() {
-    // this.read();
-  }
+  // openSettings() {
+  //   RNBluetoothClassic.openBluetoothSettings();
+  // }
 
-  openSettings() {
-    RNBluetoothClassic.openBluetoothSettings();
-  }
+  // async startScanning() {
+  //   // let granted = await this.requestAccessFineLocationPermission();
+  //   // if (!granted) {
+  //   //   throw new Error(`Access fine location was not granted`);
+  //   // }
+  //   // this.setState({ scanning: true });
+  //   // let devices = [];
+  //   // let pairedDevices = await RNBluetoothClassic.getBondedDevices();
+  //   // for (let device of pairedDevices) {
+  //   //   devices.push(device);
+  //   // }
+  //   // let connectedDevices = await RNBluetoothClassic.getConnectedDevices();
+  //   // for (let device of connectedDevices) {
+  //   //   devices.push(device);
+  //   // }
+  //   // this.setState({ devices, scanning: false });
+  // }
 
-  async startScanning() {
-    let granted = await this.requestAccessFineLocationPermission();
-    // console.log("granted", granted);
-    if (!granted) {
-      throw new Error(`Access fine location was not granted`);
+  // async pair() {
+  //   this.setState({ pairing: true });
+  //   await RNBluetoothClassic.pairDevice(this.state.devices[0].address);
+  //   this.setState({ pairing: false });
+  // }
+
+  // async unpair() {
+  //   this.setState({ unpairing: true });
+  //   await RNBluetoothClassic.unpairDevice(this.state.devices[0].address);
+  //   this.setState({ unpairing: false });
+  // }
+
+  // async connect() {
+  //   this.setState({ connecting: true });
+  //   let connection = await this.state.devices[0].connect({
+  //     DEVICE_CHARSET: Platform.OS === "ios" ? 1536 : "utf-8",
+  //   });
+
+  //   console.log("Connected :", connection);
+  //   this.setState({ connecting: false });
+  // }
+
+  // async read() {
+  //   this.setState({ reading: true });
+  //   let message = await this.state.devices[0].read();
+  //   if (message != null) {
+  //     this.setState({ message });
+  //   }
+  //   this.setState({ reading: false });
+  //   setTimeout(await this.read(), 500);
+  // }
+
+  startScanning() {
+    let scanning = this.state.scanning;
+    if (scanning) {
+      this.manager.stopDeviceScan();
+      this.setState({ scanning: false });
+    } else {
+      this.setState({ scanning: true });
+      this.manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+          return;
+        }
+        if (device.name != null) {
+          console.log("Name:", device.name);
+          console.log("Local Name:", device.localName);
+          console.log("Connectable:", device.isConnectable);
+          console.log("---------------------------------");
+        }
+      });
     }
-
-    this.setState({ discovering: true });
-
-    let devices = [];
-    // let scanResults = await RNBluetoothClassic.startDiscovery();
-
-    // for (let device of scanResults) {
-    //   if (device.name === "HC-05") {
-    //     devices.push(device);
-    //   }
-    // }
-
-    let pairedDevices = await RNBluetoothClassic.getBondedDevices();
-    for (let device of pairedDevices) {
-      devices.push(device);
-    }
-    // this.setState({ devices });
-
-    let connectedDevices = await RNBluetoothClassic.getConnectedDevices();
-    for (let device of connectedDevices) {
-      devices.push(device);
-    }
-
-    this.setState({ devices, discovering: false });
-  }
-
-  async pair() {
-    this.setState({ pairing: true });
-    await RNBluetoothClassic.pairDevice(this.state.devices[0].address);
-    this.setState({ pairing: false });
-  }
-
-  async unpair() {
-    this.setState({ unpairing: true });
-    await RNBluetoothClassic.unpairDevice(this.state.devices[0].address);
-    this.setState({ unpairing: false });
-  }
-
-  async connect() {
-    this.setState({ connecting: true });
-    let connection = await this.state.devices[0].connect({
-      DEVICE_CHARSET: Platform.OS === "ios" ? 1536 : "utf-8",
-    });
-
-    console.log("Connected :", connection);
-    this.setState({ connecting: false });
-  }
-
-  async read() {
-    this.setState({ reading: true });
-    let message = await this.state.devices[0].read();
-    if (message != null) {
-      this.setState({ message });
-    }
-    this.setState({ reading: false });
-    setTimeout(await this.read(), 500);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Button
-          title="Open Settings"
-          onPress={() => this.openSettings()}
-        ></Button>
-        <Button
-          title={!this.state.discovering ? "Start Scanning" : "Scanning..."}
-          onPress={async () => await this.startScanning()}
+          title={!this.state.scanning ? "Start Scanning" : "Stop Scanning"}
+          onPress={() => this.startScanning()}
         ></Button>
         <Button
           title={this.state.pairing ? "Pairing..." : "Pair"}
