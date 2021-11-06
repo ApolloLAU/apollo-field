7c4a7f24719440078d7ac4fa9af2e7b5d4fcc0e8
ea1ik <ea1ik@icloud.com>
2021-11-06 12:27:03 +0200

    customized main menu screen

--- screens/MainMenuScreen.js
+++ screens/MainMenuScreen.js
@@ -3,6 +3,12 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileScreen from "./ProfileScreen";
 import ChatScreen from "./ChatScreen";
 import MissionScreen from "./MissionScreen";
+import ChatScreenIcon from "../assets/svg/chat.svg";
+import ChatScreenIconHighlighted from "../assets/svg/chat-highlighted.svg";
+import MissionScreenIcon from "../assets/svg/mission.svg";
+import MissionScreenIconHighlighted from "../assets/svg/mission-highlighted.svg";
+import ProfileScreenIcon from "../assets/svg/profile.svg";
+import ProfileScreenIconHighlighted from "../assets/svg/profile-highlighted.svg";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,9 +16,45 @@ class MainMenuScreen extends Component {
   render() {
     return (
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({ route }) => ({
           headerShown: false,
-        }}
+          tabBarIcon: ({ focused }) => {
+            if (route.name === "MissionScreen") {
+              return focused ? (
+                <MissionScreenIconHighlighted
+                  width={25}
+                  height={25}
+                  fill="#000000"
+                />
+              ) : (
+                <MissionScreenIcon width={25} height={25} fill="#000000" />
+              );
+            } else if (route.name === "ProfileScreen") {
+              return focused ? (
+                <ProfileScreenIconHighlighted
+                  width={25}
+                  height={25}
+                  fill="#000000"
+                />
+              ) : (
+                <ProfileScreenIcon width={25} height={25} fill="#000000" />
+              );
+            } else {
+              return focused ? (
+                <ChatScreenIconHighlighted
+                  width={25}
+                  height={25}
+                  fill="#000000"
+                />
+              ) : (
+                <ChatScreenIcon width={25} height={25} fill="#000000" />
+              );
+            }
+          },
+          tabBarLabel: () => {
+            return null;
+          },
+        })}
       >
         <Tab.Screen name="ChatScreen" component={ChatScreen} />
         <Tab.Screen name="MissionScreen" component={MissionScreen} />
